c10359cb678e79509466f4d78231704038aba683
Leonardo Morichelli <leonardo.morichelli@unicam.it>
2023-10-26 09:51:39 +0000

    start clean

--- uniconv/clear.sh
+++ uniconv/clear.sh
@@ -4,8 +4,9 @@ sudo chmod -R 775 ./public;
 sudo chgrp -R www-data ./storage;
 sudo chmod -R 775 ./storage;
 
-sudo chgrp -R www-data ./vendor/onelogin/php-saml/certs/sp.*
-sudo chmod 640 ./vendor/onelogin/php-saml/certs/sp.*
+#sudo chgrp -R www-data ./vendor/onelogin/php-saml/certs/sp.*
+#sudo chmod 640 ./vendor/onelogin/php-saml/certs/sp.*
+sudo rm -f ./vendor/onelogin/php-saml/certs/sp.*
 
 php artisan cache:clear;
 php artisan route:clear;
